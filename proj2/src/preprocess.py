41235663bd40d4133b3c419b5c57b6175851a1e2
nathandekeyrel <natedekeyrel@gmail.com>
2024-10-03 21:46:55 -0600

    finished preprocessing

--- proj2/src/preprocess.py
+++ proj2/src/preprocess.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.feature_selection import mutual_info_classif
+
+"""
+NOTE: this does rely on some sklearn methods. I will talk to either the prof or TA (whichever is in class) to make sure
+it is ok to use them in this sense. If not, the majority of the datasets won't need feature selection (assumption)
+so I will figure out how to do it from scratch for the forest-fires dataset if needed. Because of how explicit the names
+file was about feature correlation, I think it's worth checking for that dataset.
+"""
 
 
 def preprocess_data(filepath):
@@ -25,44 +34,41 @@ def preprocess_data(filepath):
 
 
 def _preprocess_cancer(filepath):
-    """Preprocess Cancer dataset...
+    """Preprocess Breast Cancer dataset.
 
-    :param filepath:
-    :return:
+    :param filepath: Path to the data file
+    :return: Tuple of (X, y) where X is the feature matrix and y is the target vector
     """
     df = pd.read_csv(filepath, header=None, na_values='?')
-
+    # give columns a name (I'm slow, so it was better than trying to index into the right one)
     columns = ['Id', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape',
                'Marginal Adhesion', 'Single Epithelial Cell Size', 'Bare Nuclei',
                'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
     df.columns = columns
-    df = df.dropna()
 
+    df = df.dropna()  # there are 16 missing values, drop them because instance count is high
+
+    # map class values to binary
     class_mapping = {
         2: 0,
         4: 1
     }
+    y = df['Class'].map(class_mapping).values
 
-    df['Class'] = df['Class'].map(class_mapping)
-
-    # could add .values here, depending on if we standardize or not.
+    # drop 'Id' (useless) and 'Class' (target)
     X = df.drop(['Id', 'Class'], axis=1)
-    y = df['Class']
 
-    # we might need to standardize numerical features,
-    # https://scikit-learn.org/dev/modules/generated/sklearn.preprocessing.StandardScaler.html
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
 
-    # scalar = StandardScaler()
-    # X = scalar.fit_transform(X)
-
-    return X, y.values
+    return X, y
 
 
 def _preprocess_glass(filepath):
-    """Preprocess Glass dataset...
+    """Preprocess Glass dataset.
 
-    :param filepath:
-    :return:
+    :param filepath: Path to the data file.
+    :return: Tuple of (X, y) where X is the feature matrix and y is the target vector
     """
     df = pd.read_csv(filepath, header=None, na_values='?')
 
@@ -71,20 +77,22 @@ def _preprocess_glass(filepath):
 
     # drop id (useless) and type (the target)
     X = df.drop(['Id', 'Type'], axis=1)
-    # Target is 'Type'
-    y = df['Type']
+    # Target is 'Type' is valued from 1-7 (integers)
+    # 4 vehicle_windows_non_float_processed (none in this database)
+    y = df['Type'].values
 
-    # scalar = StandardScaler()
-    # X = scalar.fit_transform(X)
+    # the other classes are numeric, standardize
+    scalar = StandardScaler()
+    X = scalar.fit_transform(X)
 
-    return X, y.values
+    return X, y
 
 
 def _preprocess_soybean(filepath):
-    """Preprocess Soybean dataset...
+    """Preprocess Soybean dataset.
 
-    :param filepath:
-    :return:
+    :param filepath: Path to the data file.
+    :return: Tuple of (X, y) where X is the feature matrix and y is the target vector
     """
     df = pd.read_csv(filepath, header=None)
 
@@ -96,6 +104,7 @@ def _preprocess_soybean(filepath):
                'Shriveling', 'Roots', 'Class']
     df.columns = columns
 
+    # these columns either have values of all 0 or all 1. depending on outcome we may or may not keep this.
     columns_to_drop = [
         'Date', 'Temp', 'Leafspots Halo', 'Leaf Shread', 'Leaf Malf', 'Leaf Mild',
         'Stem', 'Seed', 'Mold Growth', 'Seed Discolor', 'Seed Size', 'Shriveling'
@@ -110,7 +119,18 @@ def _preprocess_soybean(filepath):
 
     y = df['Class'].map(class_mapping).values  # target is 'Class'
 
-    X = df.drop([columns_to_drop, 'Class'], axis=1)
+    # temporary to check feature importance
+    X = df.drop('Class', axis=1)
+
+    # check feature importance
+    mi_scores = mutual_info_classif(X, y)
+    mi_scores = pd.Series(mi_scores, index=X.columns)
+    print("\nFeature importance:")
+    print(mi_scores.sort_values(ascending=False))
+
+    # final X will be determined by mi_scores output.
+
+    # X = df.drop([columns_to_drop, 'Class'], axis=1)
     encoder = OneHotEncoder(handle_unknown='ignore')
     X_encoded = encoder.fit_transform(X)
 
@@ -123,8 +143,8 @@ def _preprocess_soybean(filepath):
 def _preprocess_abalone(filepath):
     """Preprocess Abalone dataset...
 
-    :param filepath:
-    :return:
+    :param filepath: Path to the data file.
+    :return: Tuple of (X, y) where X is the feature matrix and y is the target vector
     """
     df = pd.read_csv(filepath, header=None)
 
@@ -132,26 +152,41 @@ def _preprocess_abalone(filepath):
                'Shucked Weight', 'Viscera Weight', 'Shell Weight', 'Rings']
     df.columns = columns
 
-    # all relevant attributes
-    # no missing values
-
-    # we need to one-hot code Sex
-
-    X = df.drop(['Rings'], axis=1)
     # Target: 'Rings' --> Age = Rings + 1.5
-    y = df['Rings']
+    y = df['Rings'].values
 
-    # scalar = StandardScaler()
-    # X = scalar.fit_transform(X)
+    # one-hot encode only the 'Sex' column
+    encoder = OneHotEncoder(handle_unknown='ignore')
+    sex_encoded = encoder.fit_transform(df[['Sex']])
+    sex_column_names = encoder.get_feature_names_out(['Sex'])
 
-    return X, y.values
+    # define numeric columns
+    numeric_columns = ['Length', 'Diameter', 'Height', 'Whole Weight',
+                       'Shucked Weight', 'Viscera Weight', 'Shell Weight']
+
+    # new dataframe with encoded 'Sex' and numeric columns
+    X = pd.concat([
+        pd.DataFrame(sex_encoded, columns=sex_column_names),
+        df[numeric_columns]
+    ], axis=1)
+
+    scaler = StandardScaler()
+    X[numeric_columns] = scaler.fit_transform(X[numeric_columns])
+
+    # check feature importance
+    mi_scores = mutual_info_classif(X, y)
+    mi_scores = pd.Series(mi_scores, index=X.columns)
+    print("\nFeature importance:")
+    print(mi_scores.sort_values(ascending=False))
+
+    return X.values, y
 
 
 def _preprocess_fires(filepath):
     """Preprocess Forest Fires dataset...
 
-    :param filepath:
-    :return:
+    :param filepath: Path to the data file.
+    :return: Tuple of (X, y) where X is the feature matrix and y is the target vector
     """
     df = pd.read_csv(filepath)
 
@@ -159,21 +194,43 @@ def _preprocess_fires(filepath):
                'ISI', 'temp', 'RH', 'wind', 'rain', 'area']
     df.columns = columns
 
-    # all attributes seem relevant.
-    # no missing values
-
-    # Note: several of the attributes may be correlated, thus it makes sense to apply some sort of feature selection.
-
     # Target: area - the burned area of the forest (in ha): 0.00 to 1090.84
     #                (this output variable is very skewed towards 0.0, thus it may make
     #                sense to model with the logarithm transform)
+    y = np.log1p(df['area'])
+    # I used log1p because "This function is particularly useful when xxx is close to zero"
+    # ref - https://medium.com/@noorfatimaafzalbutt/understanding-np-log-and-np-log1p-in-numpy-99cefa89cd30
+    # documentation - https://numpy.org/doc/2.0/reference/generated/numpy.log1p.html
+
+    # one-hot encode 'month' and 'day'
+    encoder = OneHotEncoder(handle_unknown='ignore')
+    categorical_encoded = encoder.fit_transform(df[['month', 'day']])
+    categorical_column_names = encoder.get_feature_names_out(['month', 'day'])
+
+    numeric_columns = df.drop(['month', 'day', 'area'], axis=1).columns
+
+    X = pd.concat([
+        pd.DataFrame(categorical_encoded, columns=categorical_column_names),
+        df[numeric_columns]
+    ], axis=1)
+
+    scaler = StandardScaler()
+    X[numeric_columns] = scaler.fit_transform(X[numeric_columns])
+
+    # Note: several of the attributes may be correlated, thus it makes sense to apply some sort of feature selection.
+    mi_scores = mutual_info_classif(X, y)
+    mi_scores = pd.Series(mi_scores, index=X.columns)
+    print("\nFeature importance:")
+    print(mi_scores.sort_values(ascending=False))
+
+    return X.values, y.values
 
 
 def _preprocess_computer(filepath):
     """Preprocess Computer dataset...
 
-    :param filepath:
-    :return:
+    :param filepath: Path to the data file.
+    :return: Tuple of (X, y) where X is the feature matrix and y is the target vector
     """
     df = pd.read_csv(filepath, header=None)
 
@@ -181,8 +238,21 @@ def _preprocess_computer(filepath):
                'MMAX', 'CACH', 'CHMIN', 'CHMAX', 'PRP', 'ERP']
     df.columns = columns
 
-    # can probably drop vendor name and model name. unlikely to add anything. Check 'ERP'
-    # no missing values
+    y = df['PRP'].values  # Target: 'PRP', continuously valued
 
-    # Target: 'PRP', continuously valued
+    # drop vendor name, model name, and erp. they don't actually tell us anything about the computer
+    # also dropped PRP as it's the target.
+    numeric_columns = df.drop(['Vendor Name', 'Model Name', 'PRP', 'ERP'], axis=1).columns
+    X = df[numeric_columns]
 
+    # standardize continuous values
+    scaler = StandardScaler()
+    X = pd.DataFrame(scaler.fit_transform(X), columns=numeric_columns)
+
+    # at least check if correlation. Unlikely we need it here.
+    mi_scores = mutual_info_classif(X, y)
+    mi_scores = pd.Series(mi_scores, index=X.columns)
+    print("\nFeature importance:")
+    print(mi_scores.sort_values(ascending=False))
+
+    return X.values, y
