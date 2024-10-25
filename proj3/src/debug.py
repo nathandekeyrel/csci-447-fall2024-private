5b9ba003a64440d8ca6c97ef1cd19aa7892fbd05
comradealek <aleksandr.means@zoho.com>
2024-10-24 20:31:47 -0600

    updates

--- proj3/src/debug.py
+++ proj3/src/debug.py
@@ -3,7 +3,10 @@ import ffNN as fn
 import evaluating as ev
 import tenfoldcv as xv
 import utils as ut
+import numpy as np
 from numpy import mean, square
+
+np.seterr(all='raise')
 
 def r2(y_true, y_pred):
   m = mean(y_true)
@@ -15,17 +18,25 @@ def r2(y_true, y_pred):
   r = 1 - (e2_s / v2_s)
   return r
 
-X, Y = pr.preprocess_data("data/abalone.data")
+X, Y = pr.preprocess_data("data/machine.data")
 
 X_train, Y_train, X_test, Y_test = ut.generateTestData(X, Y)
 
 re = fn.ffNNRegression(X_train, Y_train, len(X[0]), len(X[0]) * 2, 3)
+# cl = fn.ffNNClassification(len(X[0]), len(X[0]) * 2, 1, len(Y[0]))
 
+# f = open("output/soybean.csv", 'w')
+ept = 100
 epochs = 0
 while True:
-  epochs += 100
-  re.train(100, 100, .05, 1)
+# for i in range(1000):
+  epochs += ept
+  re.train(ept, 100, .0001, 1)
   p = re.predict(X_test)
+  # cl.train(X_train, Y_train, ept, 10, 0.01, 0.0)
+  # p = cl.predict(X_test)
   print("epoch: " + str(epochs))
   print(ev.mse(Y_test, p))
   print(r2(Y_test, p))
+  # Y_true = np.array([np.argmax(y) for y in Y_test])
+  # f.write(str(epochs) + "," + str(ev.zero_one_loss(Y_true, p)) + "\n")
